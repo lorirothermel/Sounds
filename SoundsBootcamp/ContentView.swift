3deda7800c89cdc0c1229171bd68e28d11dd28c8
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-01 15:48:07 -0400

    Initial Commit

---
+++ SoundsBootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SoundsBootcamp
+//
+//  Created by Lori Rothermel on 4/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoundsBootcamp/SoundsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundsBootcampApp.swift
+//  SoundsBootcamp
+//
+//  Created by Lori Rothermel on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SoundsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
